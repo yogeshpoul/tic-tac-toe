f6a52e98deaa61eb594140ec4d2ba82598be53ac
Yogesh Poul Patil <96036197+yogeshpoul@users.noreply.github.com>
2024-03-17 15:44:46 +0530

    Update App.jsx

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -128,7 +128,7 @@ const App = () => {
     const username = result.value;
     setPlayerName(username);
 
-    const newSocket = io("http://localhost:3000", {
+    const newSocket = io("https://tic-tac-toe-fs.vercel.app/", {
       autoConnect: true,
     });
 
